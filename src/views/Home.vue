61fd3f94cd76f492acb09d3707b8fa31a54e2eb5
old2son <caiye2531889700@gmail.com>
2023-03-17 10:37:27 +0800

    click init pineapple

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { usePineappleStore  } from '@/stores/Pineapplestore';
-import { useCounterStore  } from '@/stores/CounterStore';
+import { storeToRefs } from 'pinia';
 import getPineapple from '@/components/Getpineapple.vue';
-import { storeToRefs } from 'pinia'
+import trash from '@/components/Trash.vue';
 
 const pineappleStore = usePineappleStore();
 const { count, rank, double } = storeToRefs(pineappleStore);
-const counterStore = useCounterStore();
 const $counter = ref();
 const $rank = ref();
 
@@ -16,7 +15,7 @@ const $rank = ref();
 <template>
     <div class="container">
         <get-pineapple></get-pineapple>
-        <!-- <div ref="$counter"><span style="font-size: 30px;">🍍</span>粉碎数:{{ count }}</div>
+        <div ref="$counter"><span style="font-size: 30px;">🍍</span>粉碎数:{{ count }}</div>
         <div ref="$counter"><span style="font-size: 30px;">🍍</span>粉碎数x2:{{ double }}</div>
         <a>记录</a>
         <ul ref="$rank">
@@ -24,7 +23,8 @@ const $rank = ref();
             <template v-for="(item, index) in rank" :key="index">
                 <li>{{ item.name }}：{{ item.score }} <i @click.prevent="">X</i></li>
             </template>
-        </ul> -->
+        </ul>
+        <trash :msg="'?'"></trash>
     </div>
 </template>
 
